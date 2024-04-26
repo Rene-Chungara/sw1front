62164b8270bfcb41bfd80ffba21a992953fa62f4
Rene E. Chungara <renechungara03@gmail.com>
2024-04-26 09:01:45 -0400

    Cambio 5.0

--- src/index.js
+++ src/index.js
@@ -4,22 +4,16 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const flash = require('connect-flash');
 const session = require('express-session');
-// const validator = require('express-validator');
-
-const MySQLStore = require('express-mysql-session');
 const passport = require('passport');
-// const bodyParser = require('body-parser');
-//  const { engine } = require('express-handlebars');
+const pgSession = require('connect-pg-simple')(session);
+
 const app = express();
 require('./lib/passport');
 
 const { database } = require('./keys');
 
 // Settings
-
 app.set('port', process.env.PORT || 5000);
-
-//__dirname es una constantes que me devuelve la direccion de la carpeta
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs.engine({
   defaultLayout: 'main',
@@ -36,26 +30,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(session({
+  store: new pgSession({
+    pool: database, // Aquí pasas directamente la configuración del pool de PostgreSQL
+    tableName: 'session' // Nombre de la tabla donde se almacenarán las sesiones
+  }),
   secret: 'msm',
   resave: false,
   saveUninitialized: false,
-  store: new MySQLStore(database)
 }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(validator());
-
-
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(bodyParser.json());
 
 // Global variables
 app.use((req, res, next) => {
   app.locals.message = req.flash('message');
   app.locals.success = req.flash('success');
   app.locals.user = req.user;
-  //  app.locals.idSALA = req.
   next();
 });
 
