c88f434435411f4afa70ccde2481881e29294f5b
Rene E. Chungara <renechungara03@gmail.com>
2024-04-26 03:26:29 -0400

    base de datos postgres

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql');
+const pg = require('pg');
 //para las promesas
 const { promisify } = require('util');
 
 const { database } = require('./keys');
 
-const pool = mysql.createPool(database);
+const pool = pg.createPool(database);
 
 pool.getConnection((err, connection) => {
     if (err) {
