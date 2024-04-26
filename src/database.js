da225c4cf05f4e063f0c84173e19d8495b99f4a5
Rene E. Chungara <renechungara03@gmail.com>
2024-04-26 08:11:33 -0400

    update db

--- src/database.js
+++ src/database.js
@@ -1,38 +1,15 @@
-const {Pool} = require('pg');
-//para las promesas
-const { promisify } = require('util');
-
+const { Pool } = require('pg');
 const { database } = require('./keys');
 
-const pool = new Pool({
-    user: database.user,
-    host: database.host,
-    database: database.database,
-    password: database.password,
-    port: database.port,
+const pool = new Pool(database);
+
+pool.on('connect', () => {
+    console.log('Connected to PostgreSQL database');
 });
 
-pool.getConnection((err, connection) => {
-    if (err) {
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Database connection fue cerrada.');
-        }
-        if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has to many connections');
-        }
-        //fue rechazada
-        if (err.code === 'ECONNREFUSED') {
-            console.error('Database connection fue rechazada');
-        }
-    }
-
-    if (connection) {connection.release();
-    console.log('DB esta conectada');
-    }
-    return;
+pool.on('error', (err) => {
+    console.error('Unexpected error on idle PostgreSQL client', err);
+    process.exit(-1);
 });
-
-// Promisify Pool Querys
-pool.query = promisify(pool.query);
 
 module.exports = pool;
